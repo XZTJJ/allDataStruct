71c9c0071486ad2a0ae53df0170cb483274a5413
XZTJJ <2734788393@qq.com>
2019-07-29 23:40:49 +0800

    代码测试

---
+++ linearTable/src/main/java/com/zhouhc/datastruct/JustTestClass.java
@@ -0,0 +1,21 @@
+package com.zhouhc.datastruct;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * @ClassName: JustTestClass
+ * @Description: TODO
+ * @Author: zhouhc
+ * @CreateDate: 2019/7/29 23:36
+ */
+public class JustTestClass {
+    private  static Logger LOGGER = LogManager.getLogger(JustTestClass.class);
+
+    @Test
+    public void onlyTest(){
+        LOGGER.info("JustTestClass Test");
+    }
+
+}
